96f052dccc5c04c8fb55fe1c1534e5bf9fc934b7
Areej <ibraheem@24online.jo>
2022-07-14 16:13:25 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,11 @@ Future<void> main() async {
   runApp(const FlutterTask());
 }
 
-// see the analysis_options file , i added important rules for good coding.
-// api's is not working yet because they Expired. if we tried another api's
-// it will work normally
+
+/*
+- see the analysis_options file , i added important rules for good coding.
+- api's is not working yet because they Expired. if we tried another api's
+ it will work normally.
+- code is inside (dev) branch.
+ */
+
